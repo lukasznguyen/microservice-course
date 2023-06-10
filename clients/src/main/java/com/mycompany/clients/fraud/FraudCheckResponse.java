db6b284ebf1ad4eb0c05e6e66771b69457a2f14d
Luke Nguyen <lukasznguyen1999@gmail.com>
2023-06-10 15:26:38 +0200

    Add FraudClient - OpenFeign

---
+++ clients/src/main/java/com/mycompany/clients/fraud/FraudCheckResponse.java
@@ -0,0 +1,5 @@
+package com.mycompany.clients.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+
+}
---
+++ clients/src/main/java/com/mycompany/clients/fraud/FraudClient.java
@@ -0,0 +1,13 @@
+package com.mycompany.clients.fraud;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "fraud", path = "api/v1/fraud-check")
+public interface FraudClient {
+
+    @GetMapping("/{customerId}")
+    FraudCheckResponse isFraudster(@PathVariable Integer customerId);
+
+}
